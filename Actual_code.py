6d46440d77b8706decd30d6968cef76554f04ade
irfanajmi <irfanajmi1721@gmail.com>
2026-01-06 09:26:00 +0800

    perform label encoding on subscription and gender features

--- Actual_code.py
+++ Actual_code.py
@@ -31,6 +31,13 @@ df["customer_feedback(vector)"] = df["customer_feedback"].apply(get_embedding)
 print(df)
 
 ###########################################################################################
-#start tulis code dekat sini
+#RANDOM FOREST MODEL
 #buat model
+# --- 5. CATEGORICAL MAPPING ---
+# Mapping subscription to ordinal values and gender to binary
+df['subscription_type'] = df['subscription_type'].map({'Basic': 1, 'Standard': 2, 'Premium': 3})
+df['gender'] = df['gender'].map({'M': 1, 'F': 0})
 
+# Drop raw text and ID (we have vectors and ID isn't a feature)
+df.drop(columns=["customer_feedback", "customer_id"], inplace=True)
+print("✅ Step 5: Categorical data mapped and IDs dropped.")
