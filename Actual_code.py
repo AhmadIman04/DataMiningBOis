b9fa19713ed3354947807d1fedd9480576194435
irfanajmi <irfanajmi1721@gmail.com>
2026-01-06 09:30:23 +0800

    initialize and train RandomForestClassifier

--- Actual_code.py
+++ Actual_code.py
@@ -54,3 +54,14 @@ y = df['churn']
 # Split data (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print(" Features fused and data split for training.")
+
+# --- 7. RANDOM FOREST MODELING ---
+print("Training Random Forest... this might take a moment.")
+model_rf = RandomForestClassifier(
+    n_estimators=100, 
+    random_state=42, 
+    class_weight='balanced'
+)
+
+model_rf.fit(X_train, y_train)
+print("Random Forest model training complete.")
